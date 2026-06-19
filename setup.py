7c209b7b1ae41a2b9cb1a4005ff9093bb9ff47b5
Sehaj <sehajdeep.singh@ibexpayroll.ca>
2026-06-19 09:12:54 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Harmanpreet kaur",
+    author_email="hkpui1234@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
